d2e8384ffaaedb8aa3ccf9c91a6a3b530797f1a4
hanshimawan <9119.hans@smp.kanisius.edu>
2021-09-28 14:17:53 +0700

    dirapiin

--- static/script.js
+++ static/script.js
@@ -40,5 +40,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
