1873192f2b558d179146ad861453610e40acf0f8
DarrenAlex <43137267+DarrenAlex@users.noreply.github.com>
2021-10-06 20:15:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -795,7 +795,7 @@ def index():
             return render_template('index.html', title='Pendaftaran', sisacatur = catur_sisa, sisafilm = film_sisa, sisabasketputra = basketputra_sisa, sisabasketputri = basketputri_sisa, sisaband = band_sisa, sisadance = dance_sisa, sisafoto = foto_sisa, sisadebat= debat_sisa, sisapidato = pidato_sisa, sisakosong = kosong_sisa, sisasenjata = senjata_sisa, sisaganda = ganda_sisa, sisapadus = padus_sisa, sisadesign = design_sisa)
         if time.time() < 1633167000:
             return render_template('closed.html')
-        if time.time() >= 1633744800:
+        if time.time() >= 1633759200:
             return render_template('due.html')
         if totalsisa != 0 :
             return render_template('index.html', title='Pendaftaran', sisacatur = catur_sisa, sisafilm = film_sisa, sisabasketputra = basketputra_sisa, sisabasketputri = basketputri_sisa, sisaband = band_sisa, sisadance = dance_sisa, sisafoto = foto_sisa, sisadebat= debat_sisa, sisapidato = pidato_sisa, sisakosong = kosong_sisa, sisasenjata = senjata_sisa, sisaganda = ganda_sisa, sisapadus = padus_sisa, sisadesign = design_sisa)
